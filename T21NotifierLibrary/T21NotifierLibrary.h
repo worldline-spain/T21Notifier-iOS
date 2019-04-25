8568c9ac3a773c55b1e81e96d7f03c15a610d563
Marcos Molero <marcos.molero@atos.net>
2019-04-25 12:22:13 +0200

    Add carthage integration

---
+++ T21NotifierLibrary/T21NotifierLibrary.h
@@ -0,0 +1,19 @@
+//
+//  T21NotifierLibrary.h
+//  T21NotifierLibrary
+//
+//  Created by Marcos Molero on 25/04/2019.
+//  Copyright © 2019 Worldline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for T21NotifierLibrary.
+FOUNDATION_EXPORT double T21NotifierLibraryVersionNumber;
+
+//! Project version string for T21NotifierLibrary.
+FOUNDATION_EXPORT const unsigned char T21NotifierLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <T21NotifierLibrary/PublicHeader.h>
+
+
